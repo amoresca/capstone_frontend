6060ae9a2836cc6a9bd92d141342ae572a05406a
amoresca <casee.amores@gmail.com>
2021-03-11 20:59:05 -0500

    applies theme to My Account and Requests pages

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -12,64 +12,150 @@
       />
     </header>
     <div class="main-container">
-      <img :src="user.image_url" alt="" width="50" />Profile Picture
-      <button v-on:click="openEditModal()">Edit</button>
-      <p>
-        <strong>First Name:</strong> {{ user.first_name }}
-        <button v-on:click="openEditModal()">Edit</button>
-      </p>
-      <p>
-        <strong>Last Name:</strong> {{ user.last_name }}
-        <button v-on:click="openEditModal()">Edit</button>
-      </p>
-      <p><strong>Username:</strong> {{ user.username }}</p>
-      <p>
-        <strong>Email:</strong> {{ user.email }}
-        <button v-on:click="openEditModal()">Edit</button>
-      </p>
-      <p>
-        <strong>Password:</strong> &bull;&bull;&bull;&bull;&bull;
-        <button v-on:click="openEditModal()">Edit</button>
-      </p>
-      <hr />
-      <button v-on:click="deleteUser()">Delete My Account</button>
-      <dialog>
-        <form method="dialog">
-          <h2>Edit Profile</h2>
-          <ul>
-            <li class="text-danger" v-for="error in errors" v-bind:key="error">
-              {{ error }}
-            </li>
-          </ul>
-          <div class="form-group">
-            <label>First Name:</label>
-            <input type="text" class="form-control" v-model="user.first_name" />
+      <section class="lg">
+        <div class="container">
+          <div class="table-overflow">
+            <table class="table table-xl">
+              <tbody>
+                <tr>
+                  <td><b>Profile Picture</b></td>
+                  <td
+                    ><img
+                      :src="user.image_url"
+                      alt=""
+                      class="circle"
+                      style="width:150px"
+                  /></td>
+                  <td class="text-right"
+                    ><a
+                      href="#"
+                      v-on:click.prevent="openEditModal()"
+                      class="btn btn-icon btn-success btn-circle"
+                      ><span class="fas fa-pencil-alt"></span></a
+                  ></td>
+                </tr>
+                <tr>
+                  <td><b>First Name</b></td>
+                  <td>{{ user.first_name }}</td>
+                  <td class="text-right"
+                    ><a
+                      href="#"
+                      v-on:click.prevent="openEditModal()"
+                      class="btn btn-icon btn-success btn-circle"
+                      ><span class="fas fa-pencil-alt"></span></a
+                  ></td>
+                </tr>
+                <tr>
+                  <td><b>Last Name</b></td>
+                  <td>{{ user.last_name }}</td>
+                  <td class="text-right"
+                    ><a
+                      href="#"
+                      v-on:click.prevent="openEditModal()"
+                      class="btn btn-icon btn-success btn-circle"
+                      ><span class="fas fa-pencil-alt"></span></a
+                  ></td>
+                </tr>
+                <tr>
+                  <td><b>Username</b></td>
+                  <td>@{{ user.username }}</td>
+                  <td class="text-right"></td>
+                </tr>
+                <tr>
+                  <td><b>Email</b></td>
+                  <td>{{ user.email }}</td>
+                  <td class="text-right"
+                    ><a
+                      href="#"
+                      v-on:click.prevent="openEditModal()"
+                      class="btn btn-icon btn-success btn-circle"
+                      ><span class="fas fa-pencil-alt"></span></a
+                  ></td>
+                </tr>
+                <tr>
+                  <td><b>Password</b></td>
+                  <td>&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;</td>
+                  <td class="text-right"
+                    ><a
+                      href="#"
+                      v-on:click.prevent="openEditModal()"
+                      class="btn btn-icon btn-success btn-circle"
+                      ><span class="fas fa-pencil-alt"></span></a
+                  ></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <div class="form-group">
-            <label>Last Name:</label>
-            <input type="text" class="form-control" v-model="user.last_name" />
-          </div>
-          <div class="form-group">
-            <label>Email:</label>
-            <input type="email" class="form-control" v-model="user.email" />
-          </div>
-          <hr />
-          <div class="form-group">
-            <label>New Password:</label>
-            <input type="password" class="form-control" v-model="password" />
-          </div>
-          <div class="form-group">
-            <label>Password Confirmation:</label>
-            <input
-              type="password"
-              class="form-control"
-              v-model="passwordConfirmation"
-            />
-          </div>
-          <button v-on:click.prevent="editUser()">Update</button>
-          <button>Close</button>
-        </form>
-      </dialog> </div
+          <button v-on:click="deleteUser()" class="btn btn-danger my-5 mr-10"
+            >Delete My Account</button
+          >
+          <dialog>
+            <form method="dialog">
+              <h2>Edit Profile</h2>
+              <ul>
+                <li
+                  class="text-danger"
+                  v-for="error in errors"
+                  v-bind:key="error"
+                >
+                  {{ error }}
+                </li>
+              </ul>
+              <div class="form-group">
+                <label>Profile Picture:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="user.image_url"
+                />
+              </div>
+              <div class="form-group">
+                <label>First Name:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="user.first_name"
+                />
+              </div>
+              <div class="form-group">
+                <label>Last Name:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="user.last_name"
+                />
+              </div>
+              <div class="form-group">
+                <label>Email:</label>
+                <input type="email" class="form-control" v-model="user.email" />
+              </div>
+              <hr />
+              <div class="form-group">
+                <label>New Password:</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  v-model="password"
+                />
+              </div>
+              <div class="form-group">
+                <label>Password Confirmation:</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  v-model="passwordConfirmation"
+                />
+              </div>
+              <button
+                v-on:click.prevent="editUser()"
+                class="btn btn-success-gradient m-y-10 mr-10"
+                >Update</button
+              >
+              <button>Close</button>
+            </form>
+          </dialog>
+        </div>
+      </section></div
     ><!-- / main-container -->
   </div>
 </template>
--- src/views/Requests.vue
+++ src/views/Requests.vue
@@ -1,48 +1,130 @@
 <template>
   <div class="requests" v-if="requests">
-    <div
-      class="alert alert-warning alert-dismissible fade show"
-      role="alert"
-      v-if="alert"
-    >
-      <strong>{{ alert }}</strong>
-      <button
-        type="button"
-        class="close"
-        data-dismiss="alert"
-        aria-label="Close"
-      >
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <h1>Requests</h1>
-    <h2>Borrow Requests</h2>
-    <div v-if="requests.borrow_requests.length > 0">
-      <div v-for="request in requests.borrow_requests" :key="request.id">
-        <img :src="request.requestor.image_url" alt="" width="50" />
-        <h3>
-          {{ request.requestor.first_name }} wants to borrow
-          {{ request.item.name }}.
-        </h3>
-        <!-- <img :src="request.item.image_url" alt="" width="100" /> -->
-        <button v-on:click="acceptBorrowRequest(request)">Accept</button>
-        <button v-on:click="rejectBorrowRequest(request)">Reject</button>
+    <header class="pt-200 pb-0 bg-primary dark">
+      <div class="container">
+        <h1>Requests</h1>
+        <h5 class="mb-30">Incoming!</h5>
       </div>
+      <img
+        src="/assets/images/angle-light.svg"
+        class="img-bottom mt-100"
+        alt=""
+      />
+    </header>
+    <div class="main-container">
+      <section class="lg">
+        <div class="container">
+          <div
+            class="alert alert-warning alert-dismissible fade show"
+            role="alert"
+            v-if="alert"
+          >
+            <button
+              type="button"
+              class="close"
+              data-dismiss="alert"
+              aria-label="Close"
+            >
+              <span aria-hidden="true" class="fas fa-times fs-16"></span>
+            </button>
+            <span class="fas fa-exclamation-circle fs-22 mr-15"></span>
+            <strong>{{ alert }}</strong>
+          </div>
+          <div class="row">
+            <div class="col-lg-6 pr-lg-5">
+              <h2 class="mb-50"
+                ><i class="fas fa-people-arrows fs-34 mr-5"></i> Borrow
+                Requests</h2
+              >
+              <div v-if="requests.borrow_requests.length > 0">
+                <div
+                  class="promo-box promo-left"
+                  v-for="request in requests.borrow_requests"
+                  :key="request.id"
+                >
+                  <div class="promo-container-big">
+                    <div class="promo-big">
+                      <router-link :to="`/users/${request.requestor.username}`"
+                        ><img
+                          :src="request.requestor.image_url"
+                          alt=""
+                          class="promo-box-image mb-25 raised-sm circle"
+                      /></router-link> </div
+                    ><!-- / promo-big -->
+                    <h5 class="box-title mb-15"
+                      >{{ request.requestor.first_name }}
+                      {{ request.requestor.last_name }}
+                      wants to borrow:
+                    </h5>
+                    <h6 class="box-description mb-15">{{
+                      request.item.name
+                    }}</h6>
+                    <img
+                      :src="request.item.image_url"
+                      alt=""
+                      class="rounded-10 raised-sm mb-15"
+                    />
+                    <button
+                      v-on:click="acceptBorrowRequest(request)"
+                      class="btn btn-outline-primary pill mr-25"
+                      >Accept</button
+                    >
+                    <button
+                      v-on:click="rejectBorrowRequest(request)"
+                      class="btn btn-danger pill"
+                      >Reject</button
+                    ></div
+                  ><!-- / promo-container -->
+                </div></div
+              >
+              <div v-else><h5>You have no pending borrow requests.</h5></div>
+            </div>
+            <div class="col-lg-6 pl-lg-5">
+              <h2 class="mb-80"
+                ><i class="fas fa-users fs-34 mr-5"></i> Friend Requests</h2
+              >
+              <div v-if="requests.friend_requests.length > 0">
+                <div
+                  v-for="request in requests.friend_requests"
+                  :key="request.id"
+                  class="card w-rised-icon lg-icon"
+                >
+                  <div class="card-body text-center">
+                    <div
+                      class="rised-icon bg-light circle icon-lg pos-center raised"
+                    >
+                      <img
+                        :src="request.requestor.image_url"
+                        class="circle"
+                        :alt="
+                          `${request.requestor.first_name} ${request.requestor.last_name} Profile Picture`
+                        "
+                        width="50"/></div
+                    ><!-- / rised-icon -->
+                    <h5 class="card-title mt-25"
+                      >{{ request.requestor.first_name }}
+                      {{ request.requestor.last_name }}</h5
+                    >
+                    <p class="card-text">@{{ request.requestor.username }}</p>
+                    <button
+                      v-on:click="acceptFriendship(request)"
+                      class="btn btn-outline-primary pill mr-25"
+                      >Accept</button
+                    >
+                    <button
+                      v-on:click="rejectFriendship(request)"
+                      class="btn btn-danger pill"
+                      >Reject</button
+                    > </div
+                  ><!-- / card-body -->
+                </div></div
+              >
+              <div v-else><h5>You have no pending friend requests.</h5></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-    <div v-else>You have no pending borrow requests.</div>
-    <hr />
-    <h2>Friend Requests</h2>
-    <div v-if="requests.friend_requests.length > 0">
-      <div v-for="request in requests.friend_requests" :key="request.id">
-        <img :src="request.requestor.image_url" alt="" width="50" />
-        <h3>
-          {{ request.requestor.first_name }} {{ request.requestor.last_name }}
-        </h3>
-        <button v-on:click="acceptFriendship(request)">Accept</button>
-        <button v-on:click="rejectFriendship(request)">Reject</button>
-      </div>
-    </div>
-    <div v-else>You have no pending friend requests.</div>
   </div>
 </template>
 
@@ -56,6 +138,7 @@ export default {
     return {
       requests: "",
       alert: "",
+      alertClass: "",
       errors: []
     };
   },
